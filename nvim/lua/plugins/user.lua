525efd20d1e15f7b8d0db7272bcdfeada39b0547
MeadMaker <1872247+pfassina@users.noreply.github.com>
2024-02-19 14:32:28 -0800

    Update user.lua

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -80,4 +80,21 @@ return {
       })
     end,
   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+       "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "cortex",
+          path = "~/share/cortex",
+        },
+      },
+    },
+  },
 }
